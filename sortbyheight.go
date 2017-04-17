86d3ffc9122fead7ecfb156cac49504f71eb4cee
Don B. Stringham <donbstringham@users.noreply.github.com>
2017-04-17 14:49:35 -0600

    Update

--- sortbyheight.go
+++ sortbyheight.go
@@ -1,1 +1,12 @@
 package main
+
+import "fmt"
+
+func main() {
+	var a = []int{-1, 150, 190, 170, -1, -1, 160, 180}
+	fmt.Println(sortByHeight(a))
+}
+
+func sortByHeight(a []int) []int {
+	return a
+}
